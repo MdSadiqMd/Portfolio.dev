3429ae1bc75808b5fc464d569d1689b5e8ca049d
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-11-29 20:31:30 +0530

    chore added fontawesome library

--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -1,37 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './header.css';
 
-const Header = () => {
-  return (
-    /*
-    <div className="bg-gray-800 text-white p-4">
-      <div className="flex items-center justify-between">
-        <div className="flex items-center font-bold">
-          <span className="bg-lime-600 p-2 rounded font-extrabold">S</span>
-          adiq{" "}
-          <span className="bg-lime-600 p-2 rounded font-extrabold">M</span>ohammad
+class Header extends Component {
+  state = { clicked: false };
+
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
+
+  render() {
+    return (
+      <>
+        <div id='nav'>
+          <a href='./assets'>Logo</a>
+          <div>
+            <ul id="navbar" className={this.state.clicked ? "active" : ""}>
+              <li><a className='active' href="/">Home</a></li>
+              <li><a href="/projects">Projects</a></li>
+              <li><a href="/blog">Blog</a></li>
+              <li><a href="/contact">Contact</a></li>
+            </ul>
+          </div>
+          <div id='mobile' onClick={this.handleClick}>
+            <i id="bar" className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+          </div>
         </div>
-        <div className="flex space-x-4">
-          <a href="#" className="hover:text-gray-300">Home</a>
-          <a href="#projects" className="hover:text-gray-300">Projects</a>
-          <a href="#blogs" className="hover:text-gray-300">Blogs</a>
-          <a href="#login" className="hover:text-gray-300">Login</a>
-        </div>
-      </div>
-    </div>*/
-    <div>
-      <nav className='flex'>
-        <a href="./assets/android-chrome-512x512.png">Logo</a>
-      </nav>
-      <div>
-        <ul>
-          <li><a href="">Home</a></li>
-          <li><a href="">Projects</a></li>
-          <li><a href="">Blog</a></li>
-          <li><a href="">Contact</a></li>
-        </ul>
-      </div>
-    </div>
-  );
-};
+      </>
+    );
+  }
+}
 
 export default Header;
---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+
+library.add(faBars, faTimes);
