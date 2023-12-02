3a639a7205c98586bd205312c5218cc8832498d3
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-12-02 19:35:24 +0530

    style added terminal style

--- components/Body/Terminal/terminal.tsx
+++ components/Body/Terminal/terminal.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Terminal = () => {
   return (
-    <div>terminal</div>
+    <div>
+        <p className='flex flex-row bg-[aquamarine]'><span className='w-[1vw] p-5'><hr /></span>Terminal</p>
+    </div>
   )
 }
 
