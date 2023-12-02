998e6c6ae41aa24e0d5652d4adbed52e8474125a
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-12-02 19:32:23 +0530

    refactor added title of web page

--- components/Body/Body.tsx
+++ components/Body/Body.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import About from './About/about'
+import Terminal from './Terminal/terminal'
 
 const Body = () => {
   return (
     <div>
         <About />
+        <Terminal />
     </div>
   )
 }
---
+++ components/Body/Terminal/terminal.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Terminal = () => {
+  return (
+    <div>terminal</div>
+  )
+}
+
+export default Terminal
