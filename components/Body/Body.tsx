8d9dd70afded918e5167fa6aa9a6f4f9ac324ad3
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-12-08 20:47:40 +0530

    refactor added projects section

--- components/Body/Body.tsx
+++ components/Body/Body.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import About from './About/about'
 import Terminal from './Terminal/terminal'
+import Projects from './Projects/projects'
 
 const Body = () => {
   return (
     <div>
         <About />
         <Terminal />
+        <Projects />
     </div>
   )
 }
