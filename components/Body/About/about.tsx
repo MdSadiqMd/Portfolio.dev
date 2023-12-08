47f34a1ec288b9b8a0d829a3ae4066e78ca1ebc6
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-12-08 21:12:26 +0530

    refactor replaced hr to single tag

--- components/Body/About/about.tsx
+++ components/Body/About/about.tsx
@@ -29,8 +29,7 @@ const About = () => {
                         Web3 practitioner, Full Stack developer.</p>
                 <br />                
                 <div className="socials">
-                    <hr></hr>
-                    
+                    <hr />
                     <div data-aos="zoom-in" data-aos-anchor-placement="center-bottom" data-aos-duration="1000">
                         <a href="https://github.com/MdSadiqMd" target="_blank" title="Github">
                             {/*<svg width="18" height="18" viewBox="0 0 18 18" fill="none">
@@ -93,7 +92,7 @@ const About = () => {
                             <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"/>
                         </svg>
                     </div> */}
-                    <hr></hr>
+                    <hr />
                 </div>
             </div>  
             
--- components/Body/Body.tsx
+++ components/Body/Body.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import About from './About/about'
 import Terminal from './Terminal/terminal'
-import Projects from './Projects/projects'
 
 const Body = () => {
   return (
     <div>
         <About />
         <Terminal />
-        <Projects />
     </div>
   )
 }
