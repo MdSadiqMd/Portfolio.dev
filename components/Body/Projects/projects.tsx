0ae1745ae12771dcc0e2acba1deacd79b1bbf070
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-12-08 20:49:39 +0530

    chore added tailwind material UI

--- components/Body/Projects/projects.tsx
+++ components/Body/Projects/projects.tsx
@@ -1,4 +1,14 @@
 import React from 'react'
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Button,
+  Tooltip,
+  IconButton,
+} from "@material-tailwind/react";
 
 const Projects = () => {
   return (
